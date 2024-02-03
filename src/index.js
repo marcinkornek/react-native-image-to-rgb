9a1eed634070ed69ebf96927857ca2f10fcbbff7
Marcin Kornek <marcinkornek@gmail.com>
2024-02-03 18:29:12 +0100

    chore: upadte .gitignore, add lock files, add configs

---
+++ src/index.js
@@ -0,0 +1,24 @@
+import { NativeModules, Platform } from 'react-native';
+const LINKING_ERROR =
+  `The package 'react-native-image-to-rgb' doesn't seem to be linked. Make sure: \n\n` +
+  Platform.select({ ios: "- You have run 'pod install'\n", default: '' }) +
+  '- You rebuilt the app after installing the package\n' +
+  '- You are not using Expo Go\n';
+// @ts-expect-error
+const isTurboModuleEnabled = global.__turboModuleProxy != null;
+const ImageToRgbModule = isTurboModuleEnabled
+  ? require('./NativeImageToRgb').default
+  : NativeModules.ImageToRgb;
+const ImageToRgb = ImageToRgbModule
+  ? ImageToRgbModule
+  : new Proxy(
+      {},
+      {
+        get() {
+          throw new Error(LINKING_ERROR);
+        },
+      }
+    );
+export function multiply(a, b) {
+  return ImageToRgb.multiply(a, b);
+}
